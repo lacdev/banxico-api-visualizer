83e59c10a0c73d91f33d65cc9c6867eebe6be635
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-02-28 21:47:44 -0600

    add classes object to Form component to modularize styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,21 @@
 import { useForm } from 'react-hook-form'
 
 export const Form = ({ onTokenChange, onSeriesChange, onSubmit }) => {
+  const classes = {
+    formContainer:
+      'border flex flex-col gap-4 max-w-4xl p-4  rounded-lg m-4 shadow-lg',
+    label: 'block text-gray-700 text-md font-semibold mb-1',
+    formErrors:
+      'shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline',
+    errorMessage: 'text-red-400',
+    fetchButton:
+      'px-3 py-1 w-24 cursor-pointer  border-sky-700 text-white bg-sky-700 font-semibold rounded-md',
+  }
+
   const {
     register,
     handleSubmit,
-    watch,
+    // watch,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -13,41 +24,32 @@ export const Form = ({ onTokenChange, onSeriesChange, onSubmit }) => {
     },
   })
 
-  const token = watch('token') //Watch specific input values.
+  // const token = watch('token') //Watch specific input values.
 
-  const series = watch('series') //Watch specific input values.
+  // const series = watch('series') //Watch specific input values.
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="border flex flex-col gap-4 max-w-4xl p-4  rounded-lg m-4 shadow-lg"
-    >
-      <label
-        className="block text-gray-700 text-md font-semibold mb-1"
-        htmlFor="token"
-      >
+    <form onSubmit={handleSubmit} className={classes.formContainer}>
+      <label className={classes.label} htmlFor="token">
         Banxico Token
       </label>
 
       <input
-        className="shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+        className={classes.formErrors}
         {...register('token', {
           required: 'A valid banxico token is required.',
         })}
         placeholder="a6279286a585cb47fdf7b721211477..."
         onChange={onTokenChange}
       />
-      <p className="text-red-400">{errors.token?.message}</p>
+      <p className={classes.errorMessage}>{errors.token?.message}</p>
 
-      <label
-        className="block text-gray-700 text-md font-semibold mb-1"
-        htmlFor="series"
-      >
+      <label className={classes.label} htmlFor="series">
         Series
       </label>
 
       <input
-        className="shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
+        className={classes.formErrors}
         {...register('series', {
           required:
             'Valid series for data fetching are required. Each serie needs to be comma separated. ',
@@ -55,10 +57,10 @@ export const Form = ({ onTokenChange, onSeriesChange, onSubmit }) => {
         placeholder={`SF61745,SP68257`}
         onChange={onSeriesChange}
       />
-      <p className="text-red-400">{errors.series?.message}</p>
+      <p className={classes.errorMessage}>{errors.series?.message}</p>
 
       <input
-        className="px-3 py-1 w-24 cursor-pointer  border-sky-700 text-white bg-sky-700 font-semibold rounded-md"
+        className={classes.fetchButton}
         type="submit"
         value={'Fetch'}
         onClick={onSubmit}
